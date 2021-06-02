fabb0503da2dd8332fa0129fc9137cdab3b95df4
pb7932 <pebe1928@gmail.com>
2021-06-02 12:00:14 +0200

    Creating cookies with cookie-parser

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cookieParser = require('cookie-parser');
+const {sessionManager} = require('./sessions/sessionManager');
 
 const indexRouter = require('./routes/index.routes');
+const cookieRouter = require('./routes/cookie.routes');
+const pageRouter = require('./routes/page.routes');
+
+app.use(cookieParser());
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
@@ -10,8 +16,12 @@ app.use(express.json());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-const {sessionManager} = require('./sessions/sessionManager');
-app.use(sessionManager);
 
-app.use('/', indexRouter);
+
+//app.use(sessionManager);
+
+//app.use('/', indexRouter);
+app.use('/cookies', cookieRouter);
+app.use('/', pageRouter);
+
 app.listen(3000);
